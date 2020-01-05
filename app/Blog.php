092e4a69abeb1c8403947c5c8ebb368984318001
JS <jeffreyschmitz@iMac-van-Mac.local>
2020-01-05 14:56:58 +0100

    Add instructions to Blog model

--- app/Blog.php
+++ app/Blog.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    protected $fillable = [
-        'title',
-        'content',
-        'author',
-    ];
+//    /**
+//     * Onderstaand moet je toevoegen omdat standaard Mass Protection aan staat
+//     *
+//     * @var array
+//     */
+//    protected $fillable = [
+//        'title',
+//        'content',
+//        'author',
+//    ];
+
+    /**
+     * Onderstaand geeft aan dat op geen van alle velden van de blog tabel
+     * Mass protected is. Voorwaarde is dat $fillable uitgecomment staat.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
